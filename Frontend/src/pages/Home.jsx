83594a45c993701536dc29a023ae65579529a734
NoName3755 <nmilan626@gmail.com>
2025-01-27 17:20:05 +0545

    feat: add featured properties in home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -7,13 +7,16 @@ import PropertyCard from "../components/PropertyCard";
 
 const Home = () => {
 	const [properties, setProperties] = useState([]);
+	const [featuredProperties, setFeaturedProperties] = useState([]);
 
 	useEffect(() => {
+		getFeaturedProperties();
+
 		getProperties();
 	}, []);
 
 	const getProperties = () => {
-		api.get("api/properties/")
+		api.get("api/properties/?is_featured=false")
 			.then((res) => res.data)
 			.then((data) => {
 				setProperties(data);
@@ -21,15 +24,35 @@ const Home = () => {
 			.catch((error) => console.log(error));
 	};
 
-    return (
+	const getFeaturedProperties = () => {
+		api.get("api/properties/?is_featured=true")
+			.then((res) => res.data)
+			.then((data) => {
+				setFeaturedProperties(data);
+			})
+			.catch((error) => console.log(error));
+	};
+
+	return (
 		<>
 			<Hero />
-            <h2>Properties List:</h2>
-            <div className="flex gap-x-4">
-                {properties.map((property) => {
-                    return <PropertyCard property={property} key={property.id} />;
-                })}
-            </div>
+			<h2>Featured Properties</h2>
+			<div className="flex gap-x-4">
+				{featuredProperties.map((property) => {
+					return (
+						<PropertyCard property={property} key={property.id} />
+					);
+				})}
+			</div>
+
+			<h2>Latest Properties:</h2>
+			<div className="flex gap-x-4">
+				{properties.map((property) => {
+					return (
+						<PropertyCard property={property} key={property.id} />
+					);
+				})}
+			</div>
 		</>
 	);
 };
