5cee41e1c21f87476001cb9d0f21fc4f81bf1670
NoName3755 <nmilan626@gmail.com>
2025-06-18 22:09:04 +0545

    feat: add postgresql database and collect statics

--- Backend/EstateEase/settings.py
+++ Backend/EstateEase/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from decouple import config, Csv
+import dj_database_url
 
 from pathlib import Path
 from datetime import timedelta
@@ -111,6 +112,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASE_URL = config("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(DATABASE_URL)
+
 
 AUTH_USER_MODEL =  "listings.User"
 
